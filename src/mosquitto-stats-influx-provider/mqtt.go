b136e3c95750bbe9b80a59b241cccdf51cec4af0
Andreas Maus <maus@ypbind.de>
2019-10-30 09:16:58 +0100

    Fix exit condition

--- src/mosquitto-stats-influx-provider/mqtt.go
+++ src/mosquitto-stats-influx-provider/mqtt.go
@@ -475,23 +475,23 @@ func processMessages(msg mqtt.Message) {
 		} else {
 			mqttStats.bytesSent += u
 		}
+	} else {
+	    log.WithFields(log.Fields{
+		    "mqtt.server":        config.MQTT.Server,
+		    "mqtt.client_id":     config.MQTT.ClientID,
+		    "mqtt.tls_ca":        config.MQTT.TLSCA,
+		    "mqtt.tls_cert":      config.MQTT.TLSCert,
+		    "mqtt.tls_key":       config.MQTT.TLSKey,
+		    "mqtt.insecure_ssl":  config.MQTT.InsecureSSL,
+		    "mqtt.qos":           config.MQTT.QoS,
+		    "mqtt.username":      config.MQTT.Username,
+		    "mqtt.password":      config.MQTT.mqttPassword,
+		    "mqtt.password_file": config.MQTT.PasswordFile,
+		    "message.topic":      topic,
+		    "message.payload":    payload,
+	    }).Warn("Unhandled MQTT message on statistics topic")
+	    return
 	}
-
-	log.WithFields(log.Fields{
-		"mqtt.server":        config.MQTT.Server,
-		"mqtt.client_id":     config.MQTT.ClientID,
-		"mqtt.tls_ca":        config.MQTT.TLSCA,
-		"mqtt.tls_cert":      config.MQTT.TLSCert,
-		"mqtt.tls_key":       config.MQTT.TLSKey,
-		"mqtt.insecure_ssl":  config.MQTT.InsecureSSL,
-		"mqtt.qos":           config.MQTT.QoS,
-		"mqtt.username":      config.MQTT.Username,
-		"mqtt.password":      config.MQTT.mqttPassword,
-		"mqtt.password_file": config.MQTT.PasswordFile,
-		"message.topic":      topic,
-		"message.payload":    payload,
-	}).Warn("Unhandled MQTT message on statistics topic")
-	return
 }
 
 func logStringConversionError(topic string, payload string, err error) {
